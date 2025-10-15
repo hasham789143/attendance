381e1c5c0b48e4265f8ed991e2248a83c09948e8
devops team <devopsteam95@gmail.com>
2025-10-15 07:29:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3971238517-5e0d8",
+  "appId": "1:1060779196017:web:1975b7800d56c67224e5d0",
+  "apiKey": "AIzaSyCV4zJUFg2RcHZa8nO8v8JHM_i-hqg6j8U",
+  "authDomain": "studio-3971238517-5e0d8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1060779196017"
+};
