1b221d4d1c2c68e065a366098935dbc5fdcbd17f
devops team <devopsteam95@gmail.com>
2025-10-15 07:37:02 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -38,12 +37,12 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
-       {
+      {
         protocol: 'https',
         hostname: 'avatar.vercel.sh',
-      }
+      },
     ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
