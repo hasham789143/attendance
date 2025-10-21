1804eeafd6002eb1c9a44607451437b7ac52638c
devops team <devopsteam95@gmail.com>
2025-10-21 10:00:54 +0000

    in on room  show Out of Range (1906m away)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,19 +11,27 @@ export function getDistance(
   p2: { lat: number; lng: number }
 ): number {
   const R = 6371e3; // metres
-  const φ1 = (p1.lat * Math.PI) / 180; // φ, λ in radians
-  const φ2 = (p2.lat * Math.PI) / 180;
-  const Δφ = ((p2.lat - p1.lat) * Math.PI) / 180;
-  const Δλ = ((p2.lng - p1.lng) * Math.PI) / 180;
+  const toRad = (value: number) => (value * Math.PI) / 180;
+
+  const lat1 = p1.lat;
+  const lon1 = p1.lng;
+  const lat2 = p2.lat;
+  const lon2 = p2.lng;
+
+  const dLat = toRad(lat2 - lat1);
+  const dLon = toRad(lon2 - lon1);
+  const radLat1 = toRad(lat1);
+  const radLat2 = toRad(lat2);
 
   const a =
-    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(radLat1) * Math.cos(radLat2) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
   const d = R * c; // in metres
   return d;
 }
+
 
 // Function to get a unique device ID
 export function getDeviceId(): string {
